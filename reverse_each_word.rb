c1404ba0fc6b0e8edd50368803b1816d8414cd71
david landau <thelandaus@gmail.com>
2018-05-08 19:15:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 def reverse_each_word(string)
-  string_array = string.split(' ')
-  reversed = []
-  string_array.each do |word|
-    reversed.push(word.reverse)
-  end
-  reversed.join(" ")
-end
-  
+#  string_array = string.split(' ')
+#  reversed = []
+#  string_array.each do |word|
+#    reversed.push(word.reverse)
+#  end
+#  reversed.join(" ")
+#end
+
   string.split(' ').collect do |word|
     word.reverse
   end
