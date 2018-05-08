7cc39938468dc79d43ce087bef76898519fa1cec
david landau <thelandaus@gmail.com>
2018-05-08 18:56:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ def reverse_each_word(string)
   reversed.join(" ")
 end
 
-def reverse_each_word(string)
+def reverse_each_word_with_collect(string)
   string.split(' ').collect do |word|
     word.reverse
   end
